0a357db01dc35b82d657076659b5e6e2b58f84e4
traviskuhl <traviskuhl@users.noreply.github.com>
2023-06-03 02:15:58 +0000

    deploy: ba1df587ed10f90097503855fb3074df1e59a93e

---
+++ _next/static/qaCThgCjhbpiJRiEblQIK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-9f82d295597eddc8.js","static/chunks/pages/index-7c3afb6007883104.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
